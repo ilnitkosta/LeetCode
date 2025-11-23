d2df5468c342c9fa239bad431d8faf688fee11f3
ilnitkosta <ilnitkosta@gmail.com>
2025-11-23 17:43:54 +0100

    commit 19

---
+++ Easy/n2798.py
@@ -0,0 +1,9 @@
+from typing import List
+def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+    res = 0
+
+    for i in hours:
+        if i >= target:
+            res += 1
+        
+    return res
