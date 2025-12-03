295715d6d8b7913eff3a3f32c10b7dee2173eefa
ilnitkosta <ilnitkosta@gmail.com>
2025-12-03 22:59:07 +0100

    commit

---
+++ Easy/n2235.swift
@@ -0,0 +1,3 @@
+func sum(_ num1: Int, _ num2: Int) -> Int {
+    return num1 + num2
+}
--- Easy/n709.py
+++ Easy/n709.py
@@ -2,4 +2,3 @@ def toLowerCase(self, s: str) -> str:
     return s.lower()
 
 print(toLowerCase(None, "Hello, Python!")) # Example
-
