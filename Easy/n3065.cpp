1d6effb6f297513ac16fb4de301f3646896c2aa9
ilnitkosta <ilnitkosta@gmail.com>
2025-12-10 12:52:48 +0100

    Minimum Operations to Exceed Threshold Value I

---
+++ Easy/n3065.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int stps = 0;
+        for (int i: nums) {
+            if (i < k) stps++;
+        }
+        return stps;
+    }
+};
+
+// example usage
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 4, 5};
+    int k = 4;
+
+    int result = solution.minOperations(nums, k);
+    
+    cout << "Minimum operations: " << result << endl;
+    
+    return 0;
+}
