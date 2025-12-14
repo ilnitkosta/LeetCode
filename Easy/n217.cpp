ea186128264c3daeecb34b8231cfcebce5ca13d1
ilnitkosta <ilnitkosta@gmail.com>
2025-12-14 23:08:37 +0100

    217. contains duplicate

---
+++ Easy/n217.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+class Solution {
+public:
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::unordered_set<int> known;
+        for (int num : nums) {
+            if (known.find(num) != known.end()) {
+                return true;
+            }
+            known.insert(num);
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution solution;
+    
+    std::vector<int> nums1 = {1, 2, 3, 1};
+    std::vector<int> nums2 = {1, 2, 3, 4};
+
+    std::cout << "Contains duplicate in nums1: " << (solution.containsDuplicate(nums1) ? "Yes" : "No") << std::endl;
+    std::cout << "Contains duplicate in nums2: " << (solution.containsDuplicate(nums2) ? "Yes" : "No") << std::endl;
+
+    return 0;
+}
