d5ddbd5df250ad7c922cf9ee5fc1561d7e8097b4
ilnitkosta <ilnitkosta@gmail.com>
2025-12-28 10:32:09 +0100

    509. perfect number

---
+++ Easy/n509.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if (num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336) return true;
+        return false;
+    }
+};
+
+int main() {
+    Solution sol;
+    // Example test cases
+    int test1 = 28;
+    int test2 = 12;
+    int test3 = 586;
+    
+    bool res1 = sol.checkPerfectNumber(test1);
+    bool res2 = sol.checkPerfectNumber(test2);
+    bool res3 = sol.checkPerfectNumber(test3);
+    
+    cout << "Is " << test1 << " a perfect number? " << (res1 ? "Yes" : "No" ) << endl;
+    cout << "Is " << test2 << " a perfect number? " << (res2 ? "Yes" : "No" ) << endl;
+    cout << "Is " << test3 << " a perfect number? " << (res3 ? "Yes" : "No" ) << endl;
+
+    return 0;
+}
