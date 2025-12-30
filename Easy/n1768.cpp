2a87cd5476244f88cd1262804884821e3ec05633
ilnitkosta <ilnitkosta@gmail.com>
2025-12-30 17:42:17 +0100

    1768. Merge Strings Alternately

---
+++ Easy/n1768.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res = "";
+        int i = 0, j = 0;
+
+        while (i < word1.size() || j < word2.size()) {
+            if (i < word1.size()) {
+                res += word1[i];
+                i++;
+            }
+            if (j < word2.size()) {
+                res += word2[j];
+                j++;
+            }
+        }
+
+        return res;
+    }
+};
+
+
+int main() {
+    Solution sol;
+    string word1 = "abc";
+    string word2 = "pqr";
+    cout << sol.mergeAlternately(word1, word2) << endl; // output: 'apbqcr'
+    return 0;
+}
