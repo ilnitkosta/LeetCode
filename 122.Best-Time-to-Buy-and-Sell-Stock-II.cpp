dd33aa2be106a29981b10a5f4937e91778917afd
ilnitkosta <ilnitkosta@gmail.com>
2026-01-02 19:15:45 +0100

    update

--- 122.Best-Time-to-Buy-and-Sell-Stock-II.cpp
+++ 122.Best-Time-to-Buy-and-Sell-Stock-II.cpp
@@ -4,19 +4,19 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0;
+        int res = 0;
         for(int i = 1; i < prices.size(); i++) {
             if(prices[i] > prices[i-1])
-                profit += prices[i] - prices[i-1];
+                res += prices[i] - prices[i-1];
         }
-        return profit;
+        return res;
     }
 };
 
 // example
 int main() {
     Solution sol;
-    vector<int> prices = {7,1,5,3,6,4};
+    vector<int> prices = {7,1,5,3,6,4}; // output 7
     int res = sol.maxProfit(prices);
     cout << res << endl;
     return 0;
