f1aa52df74d185e369bcc2f57a728a74bff06290
ilnitkosta <ilnitkosta@gmail.com>
2026-01-03 15:09:52 +0100

    557. Reverse Words in a String III

---
+++ 557.Reverse-Words-in-a-String-III.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join(word[::-1] for word in s.split())
+
+
+# example 
+sol = Solution()
+test = "Let's take Leetcode contest"
+
+print(sol.reverseWords(test)) # output 's'teL ekat edocteel tsetnoc'
