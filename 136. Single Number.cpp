8c16f4a7c870f4248592ca7d017e2f6191f0b872
ilnitkosta <ilnitkosta@gmail.com>
2026-01-04 18:38:36 +0100

    136. Single Number

---
+++ 136. Single Number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> map;
+
+        for (int i : nums) {
+            map[i]++;
+        }
+        for (auto &n : map) {
+            if (n.second == 1) return n.first;
+        }
+
+        return -1;
+    }
+};
+
+// example 
+
+int main() {
+    Solution sol;
+    vector<int> nums = {4,1,2,1,2};
+    cout << sol.singleNumber(nums) << endl;
+    return 0;
+}
