7fbc284a2a16eeeda5ba380ffb00099ed4dbd252
ilnitkosta <ilnitkosta@gmail.com>
2026-01-05 22:12:33 +0100

    14. Longest Common Prefix

---
+++ 14.Longest-Common-Prefix.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        for (int i = 0; i < strs[0].size(); ++i) {
+            char c = strs[0][i];
+            for (int j = 1; j < strs.size(); ++j) {
+                if (i >= strs[j].size() || strs[j][i] != c) {
+                    return strs[0].substr(0, i); 
+                }
+            }
+        }
+
+        return strs[0]; 
+    }
+};
+
+// example 
+int main() {
+    Solution sol;
+    vector<string> words = {"flower", "flow", "flight"};
+    cout << sol.longestCommonPrefix(words) << endl;  // output: "fl"
+}
