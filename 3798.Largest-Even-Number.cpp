f55b921ef99375c988dc2da447c553c4bbfd6e16
ilnitkosta <ilnitkosta@gmail.com>
2026-01-05 12:32:06 +0100

    3798. Largest Even Number

---
+++ 3798.Largest-Even-Number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string largestEven(string s) {
+        while (!s.empty() && s.back() == '1') {
+            s.pop_back();
+        }
+
+        return s;
+    }
+};
+
+
+// example
+int main() {
+    Solution sol;
+    string s = "121221";
+    cout << sol.largestEven(s) << endl;
+    return 0;
+}
