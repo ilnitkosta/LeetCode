c23f1fbc04a033892c496cab2f3ab6fe0c3aad91
ilnitkosta <ilnitkosta@gmail.com>
2026-01-12 12:59:22 +0100

    update

--- 53.Maximum-Subarray.cpp
+++ 53.Maximum-Subarray.cpp
@@ -21,6 +21,6 @@ public:
 int main() {
     Solution sol;
     vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
-    cout << sol.maxSubArray(nums) << endl; // output 9
+    cout << sol.maxSubArray(nums) << endl; // output 6
     return 0;
 }
