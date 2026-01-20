1a8411cb864e7179619a2add5f9e2f3d71c97933
ilnitkosta <ilnitkosta@gmail.com>
2026-01-20 20:26:03 +0100

    Create 657.Robot-Return-to-Origin.cpp

---
+++ 657.Robot-Return-to-Origin.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int u_len = 0, d_len = 0, l_len = 0, r_len = 0;
+
+        for (char move : moves) {
+            switch (move) {
+                case 'U': u_len++;break;
+                case 'D': d_len++;break;
+                case 'L': l_len++;break;
+                case 'R': r_len++;break;
+            }
+        }
+        return u_len == d_len && l_len == r_len;
+    }
+};
+
+// example
+int main() {
+    Solution sol;
+    string test1 = "UDLR";
+    string test2 = "UDUDLRLRLUD";
+    cout << sol.judgeCircle(test1) << endl; // output 1 (true)
+    cout << sol.judgeCircle(test2) << endl; // output 0 (false)
+    return 0;
+}
