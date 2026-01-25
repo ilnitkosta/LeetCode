d579f9e3997a51decd8149fae14c751d58e60103
ilnitkosta <ilnitkosta@gmail.com>
2026-01-25 11:55:43 +0100

    Create 3813.Vowel-Consonant-Score.cpp

---
+++ 3813.Vowel-Consonant-Score.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int vowelConsonantScore(string s) {
+        int v = 0, c = 0;
+        for (char ch : s) {
+            ch = tolower(ch);
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') v++;
+                else c++;
+            }
+        }
+
+        if (c == 0) return 0;
+        return v / c;
+    }
+};
+
+// example 
+int main() {
+    Solution sol;
+    string test1 = "hello";
+    string test2 = "shenzhen";
+    cout << sol.vowelConsonantScore(test1) << endl; //output 0
+    cout << sol.vowelConsonantScore(test2) << endl; //output 0
+    return 0;
+}
