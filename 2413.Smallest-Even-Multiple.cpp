8e721a070ce6ceb50c82e4d87320c4df4bb9ef56
ilnitkosta <ilnitkosta@gmail.com>
2026-01-28 17:38:51 +0100

    update #1

---
+++ 2413.Smallest-Even-Multiple.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        if ((n & 1) == 0) return n;
+        else return n*2;
+    }
+};
+
+//example
+int main() {
+    Solution sol;
+    int test1 = 5; int test2 = 6;
+    cout << sol.smallestEvenMultiple(test1) << endl; // output 10
+    cout << sol.smallestEvenMultiple(test2) << endl; // output 6
+    return 0;
+}
