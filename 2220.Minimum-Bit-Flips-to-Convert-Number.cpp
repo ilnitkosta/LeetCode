cab7055280d96b7a62e0df8533b02fdfc0128f74
ilnitkosta <ilnitkosta@gmail.com>
2026-01-28 17:38:35 +0100

    Create 2220.Minimum-Bit-Flips-to-Convert-Number.cpp

---
+++ 2220.Minimum-Bit-Flips-to-Convert-Number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+    int minBitFlips(int start, int goal) {
+        return __builtin_popcount(start ^ goal);
+    }
+};
+
+//example
+int main() {
+    Solution sol;
+    int start1 = 10; int goal1 = 7;
+    int start2 = 3; int goal2 = 4;
+
+    cout << sol.minBitFlips(start1, goal1) << endl; //output 3
+    cout << sol.minBitFlips(start2, goal2) << endl; // output 3
+    return 0;
+}
