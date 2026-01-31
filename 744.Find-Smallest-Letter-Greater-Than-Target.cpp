f2a07a5f35e9166a1725a58e158bdeecfc24c7b6
ilnitkosta <ilnitkosta@gmail.com>
2026-01-31 19:43:24 +0100

    Create 744.Find-Smallest-Letter-Greater-Than-Target.cpp

---
+++ 744.Find-Smallest-Letter-Greater-Than-Target.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(int i=0;i<letters.size();i++){
+            if(letters[i]>target)return letters[i];
+        }
+        return letters[0];
+    }
+};
+
+//example
+int main() {
+    Solution sol;
+    vector<char> letters = {'c', 'f', 'j'};
+    char target = 'a';
+    cout << sol.nextGreatestLetter(letters, target) << endl; // output 'c'
+    return 0;
+}
