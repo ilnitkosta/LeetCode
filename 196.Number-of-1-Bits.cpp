4b094d169183086917cd93458b19dcbbf2e2b2cc
ilnitkosta <ilnitkosta@gmail.com>
2026-02-01 20:56:14 +0100

    Create 196.Number-of-1-Bits.cpp

---
+++ 196.Number-of-1-Bits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        return __builtin_popcount(n);
+    }
+};
+
+//example
+int main() {
+    Solution sol;
+    int n = 11; // binary representation: 1011
+    cout << sol.hammingWeight(n) << endl; // output 3
+    return 0;
+}
