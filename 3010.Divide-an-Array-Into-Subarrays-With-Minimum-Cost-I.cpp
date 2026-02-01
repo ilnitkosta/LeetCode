164acaa5cc60bc874a45de7d15ff14fcabf7f36f
ilnitkosta <ilnitkosta@gmail.com>
2026-02-01 21:00:51 +0100

    update #1

--- 3010.Divide-an-Array-Into-Subarrays-With-Minimum-Cost-I.cpp
+++ 3010.Divide-an-Array-Into-Subarrays-With-Minimum-Cost-I.cpp
@@ -4,9 +4,8 @@ using namespace std;
 class Solution {
 public:
     int minimumCost(vector<int>& nums) {
-        int f=nums[0];
         sort(nums.begin()+1,nums.end());
-        return f+nums[1]+nums[2];
+        return nums[0]+nums[1]+nums[2];
     }
 };
 
