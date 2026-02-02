9b975091e2416d69eda6c72b621f53549ce8adb0
ilnitkosta <ilnitkosta@gmail.com>
2026-02-02 14:22:58 +0100

    Create 1877.Minimize-Maximum-Pair-Sum-in-Array.cpp

---
+++ 1877.Minimize-Maximum-Pair-Sum-in-Array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int maximum=0;
+        int left = 0;int right = nums.size() - 1;
+
+        while (left<right){
+            maximum = max(nums[left] + nums[right], maximum);
+            left +=1;
+            right-=1;
+        }
+        return maximum;
+    }
+};
+
+//example
+int main() {
+    Solution sol;
+    vector<int> nums = {3,5,2,3};
+    cout << sol.minPairSum(nums) << endl; // output 7
+    return 0;
+}
